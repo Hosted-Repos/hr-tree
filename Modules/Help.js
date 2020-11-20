69920bd41c413bd11791b9dba26b713b936408ca
Hosted-Repos <74760835+Hosted-Repos@users.noreply.github.com>
2020-11-20 18:16:21 +0530

    Add files via upload
    Added Help functionality.

---
+++ Modules/Help.js
@@ -0,0 +1,10 @@
+module.exports = function () {
+
+    console.log("Parameters to be passed : ")
+    console.log("View as List : view, Source Path , -l")
+    console.log("View as Tree : view, Source Path , -t")
+    console.log("Treefy : treefy, Source Path , Destination Path")
+    console.log("(Proper Metadata.json file is required)")
+    console.log("UnTreefy : untreefy, Source Path , Destination Path")
+
+}
