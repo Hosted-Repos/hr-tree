9d14eb5fb36fa8b54d7f074735607e8735064d91
Hosted-Repos <74760835+Hosted-Repos@users.noreply.github.com>
2020-11-20 18:30:14 +0530

    Update Errors.js
    Help functionality added.

--- Utilities/Errors.js
+++ Utilities/Errors.js
@@ -1,6 +1,15 @@
-//Invalid Arguments Error Function
+//Invalid Arguments Error Function    (bcz --D.R.Y.)
 module.exports = {
     InvalidArgumentsError : function() {
+        
         console.log("Invalid arguments. Refer help or documentation.");
+
+        console.log("Help : Parameters to be passed : ")
+        console.log("View as List : view, Source Path , -l")
+        console.log("View as Tree : view, Source Path , -t")
+        console.log("Treefy : treefy, Source Path , Destination Path")
+        console.log("(Proper Metadata.json file is required)")
+        console.log("UnTreefy : untreefy, Source Path , Destination Path")
+
     }
 }
