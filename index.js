a057dc2eea7153b0d50413b456e9878fc0abfb01
Hosted-Repos <74760835+Hosted-Repos@users.noreply.github.com>
2020-11-20 17:27:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,28 @@
 // Retrieving inputs and modules
-let input = process.argv;
-let View = require("./Modules/View.js");
-let Treefy = require("./Modules/Treefy.js");
-let Untreefy = require("./Modules/Untreefy.js");
-let Errors = require("./Utilities/Errors.js");
+function NodeTree(name,src,dest)
+{
+    let input = ["","",name,src,dest];
+    let View = require("./Modules/View.js");
+    let Treefy = require("./Modules/Treefy.js");
+    let Untreefy = require("./Modules/Untreefy.js");
+    let Errors = require("./Utilities/Errors.js");
 
 
-//Main Switch Case
-switch (input[2]) {
-    case 'view':
-        View(input[3], input[4]);
-        break;
-    case 'untreefy':
-        Untreefy(input[3], input[4]);
-        break;
-    case 'treefy':
-        Treefy(input[3], input[4]);
-        break;
-    default:
-        Errors.InvalidArgumentsError();
-        break;
+    //Main Switch Case
+    switch (input[2]) {
+        case 'view':
+            View(input[3], input[4]);
+            break;
+        case 'untreefy':
+            Untreefy(input[3], input[4]);
+            break;
+        case 'treefy':
+            Treefy(input[3], input[4]);
+            break;
+        default:
+            Errors.InvalidArgumentsError();
+            break;
+    }
 }
+
+modules.export = NodeTree
